2110db0b3ec8bbbe309549aae6ed48958148ace7
Will Lehman <william.p.lehman@usace.army.mil>
2019-09-23 15:08:50 -0700

    Thinking about the parsing of the csv file

--- HEFS_Reader/Implementations/HEFS_Reader.cs
+++ HEFS_Reader/Implementations/HEFS_Reader.cs
@@ -6,6 +6,15 @@ namespace HEFS_Reader.Implementations
 {
 	class HEFS_Reader
 	{
+		public bool readData(string data)
+		{
+			//is this zipped or not zipped?
+			//split based on new lines into rows for each element.
+			string[] newlines = { "/r/n" };
+			string[] rows = data.Split(newlines,StringSplitOptions.None);
+			//split based on comma
 
+			return false;
+		}
 	}
 }
