b8326c6ed1ed758bc782e6a1a5cd21fe1145adcb
Karl Tarbet <karl.l.tarbet@usace.army.mil>
2019-09-24 13:02:40 -0700

    add some code for downloading, and unzipping a file begin area for testing

--- HEFS_Reader/Implementations/HEFS_Downloader.cs
+++ HEFS_Reader/Implementations/HEFS_Downloader.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
 using System.Text;
 
 namespace HEFS_Reader.Implementations
@@ -16,23 +18,60 @@ namespace HEFS_Reader.Implementations
 		public string Response { get; set; }
 		public bool FetchData(HEFSRequestArgs args)
 		{
-			//System.Net.WebClient rootwc = new System.Net.WebClient();
 			string webrequest = _rootUrl;
 			webrequest += args.date;
 			webrequest += args.location;
 			webrequest += "_hefs_csv_hourly.zip";
 
-			//System.Net.WebClient wc = new System.Net.WebClient();
-			//Response = wc.DownloadString(webrequest);
-			//search for error codes in the output.
-			//Result = new ECAMResult(Response);
+            string zipFileName = Path.GetTempFileName();
+            string csvFileName = Path.GetTempFileName();
 
+            File.Delete(zipFileName);
+            File.Delete(csvFileName);
+            GetFile(webrequest, zipFileName);
+
+            Reclamation.Core.ZipFileUtility.UnzipFile(zipFileName, csvFileName);
+            Response = File.ReadAllText(csvFileName);
 			return true;
 			//new webconnection.
 
 			//_Result = new ECAMResult(response);
 		}
-	}
+
+        /// <summary>
+        /// from https://stackoverflow.com/questions/137285/what-is-the-best-way-to-read-getresponsestream
+        /// </summary>
+         static void GetFile(string url, string outputFilename)
+        {
+            HttpWebRequest httpRequest = (HttpWebRequest)WebRequest.Create(url);
+            httpRequest.Method = "GET";
+
+            // if the URI doesn't exist, an exception will be thrown here...
+            using (HttpWebResponse httpResponse = (HttpWebResponse)httpRequest.GetResponse())
+            {
+                using (Stream responseStream = httpResponse.GetResponseStream())
+                {
+                    using (FileStream localFileStream =
+                        new FileStream(outputFilename, FileMode.Create))
+                    {
+                        var buffer = new byte[4096];
+                        long totalBytesRead = 0;
+                        int bytesRead;
+
+                        while ((bytesRead = responseStream.Read(buffer, 0, buffer.Length)) > 0)
+                        {
+                            totalBytesRead += bytesRead;
+                            localFileStream.Write(buffer, 0, bytesRead);
+                        }
+                    }
+                }
+            }
+        }
+
+
+    }
+
+
 }
 //https://www.cnrfc.noaa.gov/ensembleHourlyProductCSV.php 
 //var filetoget = '/csv/'+yyyy+monnum+daynew+hh+'_'+theprod+'_hefs_csv_hourly.zip'
---
+++ tests/DownloadTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace tests
+{
+    [TestClass]
+    public class DownloadTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            /////https://www.cnrfc.noaa.gov/csv/2019092312_RussianNapa_hefs_csv_hourly.zip
+            var args = new HEFS_Reader.Implementations.HEFSRequestArgs();
+            args.location = "RussianNapa";
+            args.date = "201909231";
+
+
+
+           }
+    }
+}
