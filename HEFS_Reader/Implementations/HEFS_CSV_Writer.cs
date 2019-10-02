3193338730117ee2da86d523c92d7c4113061bad
Will Lehman <william.p.lehman@usace.army.mil>
2019-10-02 07:31:25 -0700

    converting to stringbuilder

--- HEFS_Reader/Implementations/HEFS_CSV_Writer.cs
+++ HEFS_Reader/Implementations/HEFS_CSV_Writer.cs
@@ -15,33 +15,32 @@ namespace HEFS_Reader.Implementations
 			{
 				string fileName = watershed.WatershedName.ToString() + "_" + HEFS_CSV_Parser.StringifyDateTime(watershed.Locations.First().IssueDate) + ".csv";
 				string fullPath = System.IO.Path.Combine(directoryPath, fileName);
-				//System.IO.FileStream fs = new System.IO.FileStream(fullPath, System.IO.FileMode.CreateNew, System.IO.FileAccess.Write);
-				string line = "";//consider stringbuilder.
+				StringBuilder line = new StringBuilder("GMT");
 				using (System.IO.StreamWriter sr = new System.IO.StreamWriter(fullPath))
 				{
-					line = "GMT";
 					foreach (IEnsemble e in watershed.Locations)
 					{
 						Int32 memberCounter = 1;
 						foreach (IEnsembleMember m in e.Members)
 						{
-							line += "," + e.LocationName + "_" + memberCounter;
+							line.Append("," + e.LocationName + "_" + memberCounter);
 							memberCounter++;
 						}
 					}
-					sr.WriteLine(line);
+					sr.WriteLine(line.ToString());
+					line = new StringBuilder();
 					Int32 counter = 0;
 					foreach (DateTime t in watershed.Locations.First().Members.First().Times)
 					{
-						line = t.ToString();
+						line = new StringBuilder(t.ToString());
 						foreach (IEnsemble e in watershed.Locations)
 						{
 							foreach (IEnsembleMember m in e.Members)
 							{
-								line += "," + m.Values[counter];
+								line.Append("," + m.Values[counter]);
 							}
 						}
-						sr.WriteLine(line);
+						sr.WriteLine(line.ToString());
 						counter++;
 					}
 				}
