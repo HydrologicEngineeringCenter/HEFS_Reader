4b20c1b9aec0237e3754afe7b6f74335cb6d4eda
Will Lehman <william.p.lehman@usace.army.mil>
2019-10-07 14:17:16 -0400

    filling out the dssensemblereader readdataset method

--- HEFSConverter/DssEnsembleReader.cs
+++ HEFSConverter/DssEnsembleReader.cs
@@ -10,170 +10,95 @@ using System.Threading.Tasks;
 
 namespace HEFSConverter
 {
-  class DssEnsembleReader : HEFS_Reader.Interfaces.IEnsembleReader
-  {
-
-    internal static HEFS_Reader.Interfaces.IWatershedForecast Read(string dssFileName, Watersheds waterShed,
-                                          DateTime startTime, DateTime endTime, out TimeSpan timeSpan)
-    {
-      List<HEFS_Reader.Interfaces.IEnsemble> ensembles = new List<HEFS_Reader.Interfaces.IEnsemble>();
-      var watershed = new HEFS_Reader.Implementations.WatershedForecast(ensembles, waterShed);
-
-      var sw = System.Diagnostics.Stopwatch.StartNew();
-      using (var dss = new DSSIO.DSSReader(dssFileName))
-      {
-        var dssPaths = dss.GetCondensedPathNames(); // sorted
-        if (dssPaths.Count == 0)
-        {
-          throw new Exception("Empty DSS catalog");
-        }
-        // /RUSSIANNAPA/APCC1/FLOW/01SEP2019/1HOUR/C:000002|T:0212019/
-        List<List<float>> ensembleValues = new List<List<float>>();
-        int size = dssPaths.Count;
-
-        for (int i = 0; i < size; i++)
-        {
-          DSSIO.DSSPath path = dssPaths[i];
-          string location = path.Bpart;
-          DateTime issueDate = ParseIssueDate(path.Fpart);
-          if (issueDate >= startTime && issueDate <= endTime
-            && path.Apart.ToLower() == waterShed.ToString().ToLower())
-          {
-            var ts = dss.GetTimeSeries(path.FullPath);
-            List<float> memberValues = new List<float>();
-            memberValues.AddRange(Array.ConvertAll(ts.Values, item => (float)item));
-            ensembleValues.Add(memberValues);
-            if (i == size - 1 || dssPaths[i + 1].Bpart != location)
-            {// package this ensemble
-              Ensemble e = new Ensemble(location, issueDate, ensembleValues, ts.Times.ToList());
-              ensembles.Add(e);
-              // start building next ensemble
-              ensembleValues = new List<List<float>>();
-            }
-          }
-        }
-
-        // args.location.ToString()
-      }
-      sw.Stop();
-      timeSpan = sw.Elapsed;
-      return watershed;
-    }
-
-    /// <summary>
-    /// parse issue date from part F:
-    /// C:000002|T:0212019
-    /// 
-    /// </summary>
-    /// <param name="path"></param>
-    /// <returns></returns>
-    private static DateTime ParseIssueDate(string fPart)
-    {
-      string[] tokens = fPart.Split('|')[1].Split(':');
-      int year = Convert.ToInt32(tokens[1].Substring(3));
-      string sday = tokens[1].Substring(0, 3);
-      int day = Convert.ToInt32(sday);
-      DateTime issueDate = new DateTime(year, 1, 1).AddDays(day - 1).AddHours(12);
-      return issueDate;
-    }
-
-    public IWatershedForecast Read(IHEFSReadArgs args)
-    {
-      throw new NotImplementedException();
-    }
-
-    //public IWatershedForecast Read(IHEFSReadArgs args)
-    //{
-    //  List<HEFS_Reader.Interfaces.IEnsemble> ensembles = new List<HEFS_Reader.Interfaces.IEnsemble>();
-    //  var watershed = new HEFS_Reader.Implementations.WatershedForecast(ensembles, args.WatershedLocation);
-    //  using (var dss = new DSSIO.DSSReader(args.Path))
-    //  {
-    //    var dssPaths = dss.GetCondensedPathNames(); // sorted
-    //    if (dssPaths.Count == 0)
-    //    {
-    //      throw new Exception("Empty DSS catalog");
-    //    }
-    //    //need all locations => b being *
-    //    //need all collection members who end in T: args.ForecastDate.DayOfYear + args.ForecastDate.Year
-    //    // /RUSSIANNAPA/APCC1/FLOW/01SEP2019/1HOUR/C:000002|T:0212019/
-    //    List<List<float>> ensembleValues = new List<List<float>>();
-    //    //int size = dssPaths.Count;
+	class DssEnsembleReader : HEFS_Reader.Interfaces.IEnsembleReader
+	{
 
 
+		/// <summary>
+		/// parse issue date from part F:
+		/// C:000002|T:0212019
+		/// 
+		/// </summary>
+		/// <param name="path"></param>
+		/// <returns></returns>
+		private static DateTime ParseIssueDate(string fPart)
+		{
+			string[] tokens = fPart.Split('|')[1].Split(':');
+			int year = Convert.ToInt32(tokens[1].Substring(3));
+			string sday = tokens[1].Substring(0, 3);
+			int day = Convert.ToInt32(sday);
+			DateTime issueDate = new DateTime(year, 1, 1).AddDays(day - 1).AddHours(12);
+			return issueDate;
+		}
+
+		public IWatershedForecast Read(IHEFSReadArgs args)
+		{
+			throw new NotImplementedException();
+		}
+
+		public ITimeSeriesOfEnsembleLocations ReadDataset(Watersheds watershed, DateTime start, DateTime end, string dssPath)
+		{
+			//create catalog here,
+			// loop through start times until end time and call read appropriately.
+
+			List<HEFS_Reader.Interfaces.IEnsemble> ensembles = new List<HEFS_Reader.Interfaces.IEnsemble>();
+			WatershedForecast watershedForecast = new WatershedForecast(ensembles, watershed);
+			TimeSeriesOfEnsembleLocations rval = new TimeSeriesOfEnsembleLocations();
+			IList<IWatershedForecast> watershedForecasts = rval.timeSeriesOfEnsembleLocations;
 
 
-    //    for (int i = 0; i < size; i++)
-    //    {
-    //      DSSIO.DSSPath path = dssPaths[i];
-    //      string location = path.Bpart;
-    //      DateTime issueDate = ParseIssueDate(path.Fpart);
-    //      if (issueDate >= startTime && issueDate <= endTime
-    //        && path.Apart.ToLower() == args.WatershedLocation.ToString().ToLower())
-    //      {
-    //        var ts = dss.GetTimeSeries(path.FullPath);
-    //        List<float> memberValues = new List<float>();
-    //        memberValues.AddRange(Array.ConvertAll(ts.Values, item => (float)item));
-    //        ensembleValues.Add(memberValues);
-    //        if (i == size - 1 || dssPaths[i + 1].Bpart != location)
-    //        {// package this ensemble
-    //          Ensemble e = new Ensemble(location, issueDate, ensembleValues, ts.Times.ToList());
-    //          ensembles.Add(e);
-    //          // start building next ensemble
-    //          ensembleValues = new List<List<float>>();
-    //        }
-    //      }
-    //    }
+			using (DSSIO.DSSReader dss = new DSSIO.DSSReader(dssPath))
+			{
+				DSSIO.DSSPathCollection dssPaths = dss.GetCondensedPathNames(); // sorted
+				if (dssPaths.Count == 0)
+				{
+					throw new Exception("Empty DSS catalog");
+				}
+				//need all locations => b being *
+				//List<String> locations = dssPaths.GetUniqueBParts();
+				//need all collection members who end in T: args.ForecastDate.DayOfYear + args.ForecastDate.Year
+				string previousT = dssPaths[0].Fpart.Split('|').Last().Split(':').Last();
+				string previousLoc = dssPaths[0].Bpart;
+				// /RUSSIANNAPA/APCC1/FLOW/01SEP2019/1HOUR/C:000002|T:0212019/
+				List<List<float>> ensembleValues = new List<List<float>>();
+				int size = dssPaths.Count;
 
-    //    // args.location.ToString()
-    //  }
-    //  return watershed;
-    //}
+				for (int i = 0; i < size; i++)
+				{
+					DSSIO.DSSPath path = dssPaths[i];
+					string currentLoc = path.Bpart;
+					string currentT = path.Fpart.Split('|').Last().Split(':').Last();//probably slowish operation
+					if (!previousT.Equals(currentT))//relying on sorted T and B
+					{
+						//issue time has changed, we have read a whole csv.
+						watershedForecasts.Add(watershedForecast);
+						ensembles = new List<HEFS_Reader.Interfaces.IEnsemble>();
+						watershedForecast = new WatershedForecast(ensembles, watershed);
+						previousT = currentT;
+						previousLoc = currentLoc;
+					}
 
-    public ITimeSeriesOfEnsembleLocations ReadDataset(Watersheds watershed, DateTime start, DateTime end, string dssPath)
-    {
-      //create catalog here,
-      // loop through start times until end time and call read appropriately.
+					DateTime issueDate = ParseIssueDate(path.Fpart);
+					if (issueDate >= start && issueDate <= end
+					  && path.Apart.ToLower() == watershed.ToString().ToLower())
+					{
+						var ts = dss.GetTimeSeries(path.FullPath);
+						List<float> memberValues = new List<float>();
+						memberValues.AddRange(Array.ConvertAll(ts.Values, item => (float)item));
+						ensembleValues.Add(memberValues);
+						if (i == size - 1 || dssPaths[i + 1].Bpart != currentLoc)
+						{// package this ensemble
+							Ensemble e = new Ensemble(currentLoc, issueDate, ensembleValues, ts.Times.ToList());
+							ensembles.Add(e);
+							// start building next ensemble
+							ensembleValues = new List<List<float>>();
+						}
+					}
+				}
 
-      List<HEFS_Reader.Interfaces.IEnsemble> ensembles = new List<HEFS_Reader.Interfaces.IEnsemble>();
-      WatershedForecast watershedForecast = new WatershedForecast(ensembles, watershed);
-      TimeSeriesOfEnsembleLocations rval = new TimeSeriesOfEnsembleLocations();
-      rval.timeSeriesOfEnsembleLocations.Add(watershedForecast);
-
-      using (var dss = new DSSIO.DSSReader(dssPath))
-      {
-        var dssPaths = dss.GetCondensedPathNames(); // sorted
-        if (dssPaths.Count == 0)
-        {
-          throw new Exception("Empty DSS catalog");
-        }
-        //need all locations => b being *
-        //need all collection members who end in T: args.ForecastDate.DayOfYear + args.ForecastDate.Year
-        // /RUSSIANNAPA/APCC1/FLOW/01SEP2019/1HOUR/C:000002|T:0212019/
-        List<List<float>> ensembleValues = new List<List<float>>();
-        int size = dssPaths.Count;
-        for (int i = 0; i < size; i++)
-        {
-          DSSIO.DSSPath path = dssPaths[i];
-          string location = path.Bpart;
-          DateTime issueDate = ParseIssueDate(path.Fpart);
-          if (issueDate >= start && issueDate <= end
-            && path.Apart.ToLower() == watershed.ToString().ToLower())
-          {
-            var ts = dss.GetTimeSeries(path.FullPath);
-            List<float> memberValues = new List<float>();
-            memberValues.AddRange(Array.ConvertAll(ts.Values, item => (float)item));
-            ensembleValues.Add(memberValues);
-            if (i == size - 1 || dssPaths[i + 1].Bpart != location)
-            {// package this ensemble
-              Ensemble e = new Ensemble(location, issueDate, ensembleValues, ts.Times.ToList());
-              ensembles.Add(e);
-              // start building next ensemble
-              ensembleValues = new List<List<float>>();
-            }
-          }
-        }
-      }
-      return rval;
-    }
-  }
+			}
+			watershedForecasts.Add(watershedForecast);
+			return rval;
+		}
+	}
 }
